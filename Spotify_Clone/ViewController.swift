10e3c6892ce0782e47b2e1d050ad29e9d97a0b1b
Kyle Corlett <corlettkyle5@hotmail.com>
2022-03-28 17:34:11 +1000

    Initial Commit

---
+++ Spotify_Clone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Spotify_Clone
+//
+//  Created by Kyle-Joshua Corlett on 28/3/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
